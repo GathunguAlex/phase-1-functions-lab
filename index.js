421ffef05262fa930ea3fc503b5bcc225ba14e4b
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-21 08:46:35 +0300

    attempt to complete assignment,will seek further assistance

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+  return pickUp - 42;
+}
+//distanceFromHqInBlocks()
+
+function distanceFromHqInFeet(feet) {
+  return feet * 264;
+}
+//distanceFromHqInBlocks(50)
+
+function distanceTravelledInFeet(distanceFromHqInBlocks) {
+  return distanceFromHqInBlocks - (pickup * 264);
+}
+
+function calculatesFarePrice() {
+  if distanceFromHqInFeet <= 400{
+    return 'This is on me!';
+  }
+  else { distanceFromHqInFeet } return
+  (distanceFromHqInFeet - 400 * 2);
+  
+  else if{ distanceFromHqInFeet > 2500}
+return ("cannot travel that far");
+  }
+
+
+}
+
